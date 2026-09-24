e568da8ad0302f1240c2c6aa9ba1ca1b1fc14c8f
JuanP-012 <juanpamartirico15@gmail.com>
2026-09-24 10:36:41 -0500

    cambios newmap

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,4 +1,16 @@
-def new_map():
+
+
+def new_map(num_elements, load_factor, prime=109345121):
+    
+    capacitiy = mp.next_prime(num_elements/load_factor)
+    scale = r.randint(1, prime-1)
+    shitf = r.randint(1, prime-1)
+    table = 
+
+    
+    
+
+
 
 def put():
 
