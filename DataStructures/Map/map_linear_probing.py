5fe33e42e96f0cf7a02d20951d4284c8030413de
santirodri1 <s.rodriguezg2345@uniandes.edu.co>
2026-09-24 10:35:28 -0500

    Funciones map

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,9 +1,40 @@
+from DataStructures.List import array_list as lt
+import random as r
+from DataStructures.Map import map_entry as me
+from DataStructures.Map import map_functions as mf
+from DataStructures.Map import map_functions as mp
 def new_map():
 
-def put():
+def put(my_map, key, value):
+
+    hash_value = mf.hash_value(my_map, key)
+
+    found, pos = find_slot(my_map, key, hash_value)
+
+    if found:
+        entry = lt.get_element(my_map["table"], pos)
+        me.set_value(entry, value)
+
+    else:
+        entry = me.new_map_entry(key, value)
+        lt.change_info(my_map["table"], pos, entry)
+
+        my_map["size"] += 1
+        my_map["current_factor"] = my_map["size"] / my_map["capacity"]
+
+        if my_map["current_factor"] > my_map["limit_factor"]:
+            my_map = rehash(my_map)
+
+    return my_map
 
 
-def contains():
+def contains(my_map, key):
+
+    hash_value = mf.hash_value(my_map, key)
+
+    found, pos = find_slot(my_map, key, hash_value)
+
+    return found
     
 
 def get():
