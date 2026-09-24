75b65c0053e713780908b2f759e01160dea06123
santirodri1 <s.rodriguezg2345@uniandes.edu.co>
2026-09-24 10:47:01 -0500

    Cambios finales primera entrega

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -3,7 +3,17 @@ import random as r
 from DataStructures.Map import map_entry as me
 from DataStructures.Map import map_functions as mf
 from DataStructures.Map import map_functions as mp
-def new_map():
+def new_map(num_elements, load_factor, prime=109345121):
+    
+    capacity = mf.next_prime(num_elements/load_factor)
+    scale = r.randint(1, prime-1)
+    shift = r.randint(0, prime-1)
+    table = lt.new_list(capacity)
+    
+    for i in range(capacity):
+        lt.add_last(table, None)
+    
+    return {"table": table, "capacity": capacity, "size": 0, "limit_factor": load_factor, "current_factor": 0, "scale": scale, "shift": shift}
 
 def put(my_map, key, value):
 
@@ -37,10 +47,31 @@ def contains(my_map, key):
     return found
     
 
-def get():
+def get(my_map, key):
+
+    hash_value = mf.hash_value(my_map, key)
+
+    found, pos = find_slot(my_map, key, hash_value)
+
+    if not found:
+        return None
+
+    entry = lt.get_element(my_map["table"], pos)
+    return me.get_value(entry)
 
 
-def remove():
+def remove(my_map, key):
 
+    hash_value = mf.hash_value(my_map, key)
 
-def size():
+    found, pos = find_slot(my_map, key, hash_value)
+
+    if found:
+        entry = me.new_map_entry("__EMPTY__", "__EMPTY__")
+        lt.change_info(my_map["table"], pos, entry)
+        my_map["size"] -= 1
+
+    return my_map
+
+def size(my_map):
+    return my_map["size"]
